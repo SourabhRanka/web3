93651e86c04254a5aee89f0961a806b96e463faf
Sourabh Ranka <sourabh.ranka@synerzip.com>
2018-04-18 22:21:38 +0530

    make basic add and delete operation from cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,34 @@ export class AppComponent {
     {
       "name": 'item1',
       "price" : 30,
-      "quantity" : 0
+      "quantity" : 0,
+      "selected" : false
     },
     {
       "name": 'item2',
       "price" : 40,
-      "quantity" : 0
+      "quantity" : 0,
+      "selected" : false
     },
   
     {
       "name": 'item3',
       "price" : 50,
-      "quantity" : 0
+      "quantity" : 0,
+      "selected" : false
     }];
   
     cart = [];
+
+    addToCart(index){
+      this.cart.push(this.inventory[index]);
+      this.inventory[index].selected = true;
+    }
+    removeFromCart(index){
+      this.inventory[index].quantity = 0;
+      let i = this.cart.indexOf(this.inventory[index]);
+      this.cart.splice(i,1);
+      this.inventory[index].selected = false;
+    }
   
 }
